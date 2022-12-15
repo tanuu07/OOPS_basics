2494f5c5e31b81e1ea65fc56c85b51e48ebb7dbc
tanuu07 <95824978+tanuu07@users.noreply.github.com>
2022-12-15 14:47:09 +0530

    Add files via upload

---
+++ constructor.cpp
@@ -0,0 +1,34 @@
+// #include<iostream>
+// using namespace std;
+// class Employee{
+// public:
+//         string Name;
+//         int Age;
+//         string Company_name;
+//         double Id_no;
+//         void employee_detail(){
+//             cout << "Name : " << Name << endl;
+//             cout << "Age : "  << Age << endl;
+//             cout << "Company Name : " << Company_name << endl;
+//             cout << "ID No : " << Id_no << endl; 
+//         }
+//         Employee(string name, int age, string company_name, double id_no){
+//             Name = name;
+//             Age = age;
+//             Company_name = company_name;
+//             Id_no = id_no;
+//         }
+// };
+// int main(){
+//     Employee employee1 = Employee("Alia", 25 ,"Tesla", 123456);
+//     employee1.employee_detail();
+//     return 0;
+// }
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    cin >> a;
+    cout << "int"<<a;
+    return 0;
+}
